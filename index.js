3c2a891349e9edc305573ceee72361e1a5cc7581
shivamkarn <shivamkarn767@gmail.com>
2025-11-01 00:13:29 +0545

    Add URL redirection logic and update route path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import urlRoute from "./routes/url.routes.js";
 import { connectDB } from "./connectDB.js";
 import dotenv from "dotenv";
-
+import { URL } from "./models/url.model.js";
 dotenv.config(); // load MONGODB_URL from .env
 
 const app = express();
@@ -18,5 +18,29 @@ connectDB().then(() => {
 app.get("/", (req, res) => {
   res.json({ message: "Server is running Great!" });
 });
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    { shortId },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    },
+    { new: true }
+  );
 
-app.use("/api/url", urlRoute);
+  if (!entry) {
+    return res.status(404).json({ message: "short id not found" });
+  }
+  let redirectTo = entry.redirectURL || "";
+  if (!/^https?:\/\//i.test(redirectTo)) {
+    redirectTo = `https://${redirectTo}`;
+  }
+
+  return res.redirect(redirectTo);
+});
+
+app.use("/url", urlRoute);
